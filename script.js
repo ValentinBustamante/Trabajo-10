a6d4a750aa587cdaae9b84d886c69344e12a8dba
ValentinBustamante <140023829+ValentinBustamante@users.noreply.github.com>
2023-08-31 20:44:06 -0300

    trabajo-10

---
+++ script.js
@@ -0,0 +1,46 @@
+let precioFinal = helado + kinder;
+console.log("El helado cuesta");
+if (helado = 300) {
+    console.log("$300");
+} else if (oreo = 10) {
+    console.log("$310");
+} else if (kikat = 15) {
+    console.log("$315");
+} else if (kinder = 20) {
+    console.log("$320");
+} else {
+    console.log("no tenemos este topping");
+}
+
+let pedido = "carne"
+switch (pedido) {
+    case "carne":
+        console.log("vino tinto");
+        break;
+    case "verdura":
+        console.log("agua");
+        break;
+    case "pescado":
+        console.log("vino blanco");
+        break;
+        defeult:
+        console.log("elija carne, verdura, pescado");
+}
+
+let paises = ["Jamaica", "Ecuador", "Argentina", "Rusia", "Nigeria"];
+console.log(paises);
+console.log(paises[0]);
+console.log(paises[4]);
+console.log("Cantidad de paises:", paises.length);
+for (let i = -100; i < paises.length; i++) {
+    console.log(paises[i])
+}
+let numero = 1;
+
+while (numero <= 10) {
+    console.log("numero es más chico que 11")
+	numero++;
+    console.log(numero)
+    
+}
+
